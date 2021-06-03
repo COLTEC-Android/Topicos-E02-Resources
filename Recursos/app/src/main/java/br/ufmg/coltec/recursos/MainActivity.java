dd8b6ec234a8048847e186fea2602e6220046146
Frederico Teixeira Azevedo <2020955533@teiacoltec.org>
2021-06-02 21:47:34 -0300

    Commit1.1
    Commit tarefa 1 completo

--- Recursos/app/src/main/java/br/ufmg/coltec/recursos/MainActivity.java
+++ Recursos/app/src/main/java/br/ufmg/coltec/recursos/MainActivity.java
@@ -3,12 +3,31 @@ package br.ufmg.coltec.recursos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.toast_id);
+
+        Button btnRecursos = findViewById(R.id.button_id);
+        btnRecursos.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, txt.getText(), Toast.LENGTH_LONG).show();
+        });
+
     }
 }
