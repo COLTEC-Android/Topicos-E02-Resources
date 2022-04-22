f456e513541d862ef3fed508f38f6a4cae59d595
2021953372 <brizaelvs@gmail.com>
2022-04-22 20:42:50 -0300

    Topicos-E02-Resources - Gabriel Vieira

--- app/src/main/java/br/ufmg/coltec/olamundo/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/olamundo/MainActivity.java
@@ -2,10 +2,12 @@ package br.ufmg.coltec.olamundo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.view.Gravity.CENTER;
@@ -18,11 +20,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnSurprise = findViewById(R.id.btn_surprise);
+
         btnSurprise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                TODO(#1): Esse texto não deveria estar aqui
-                Toast toast = Toast.makeText(MainActivity.this, "Bem vindo, mestre", Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(MainActivity.this, R.string.app_bemVindo, Toast.LENGTH_LONG);
                 toast.setGravity(CENTER, 0, 0);
                 toast.show();
             }
